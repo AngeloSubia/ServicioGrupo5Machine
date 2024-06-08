bb82f3aec6c0d187da3a054e9414c3b6b6250c0c
AngeloSubia <kurozaki258@gmail.com>
2024-06-08 17:49:39 -0500

    Update ServicioGrupo5.js

--- ServicioGrupo5.js
+++ ServicioGrupo5.js
@@ -3,12 +3,12 @@ import { createServer } from 'node:http';
 
 const server = createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!\n');
+  res.end('Grupo 5 ya estamos aqui!\n');
 });
 
 // starts a simple http server locally on port 3000
-server.listen(3000, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:3000');
+server.listen(3000, '0.0.0.0', () => {
+  console.log('Listening on 0.0.0.0:8095');
 });
 
 // run with `node server.mjs`
